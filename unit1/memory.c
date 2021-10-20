07dbc306afde3386be6b84adddbab139ed129b78
Peter Strawn <peter.strawn@jesuithighschool.org>
2021-10-20 16:46:00 +0000

    Fix memory example

--- unit1/memory.c
+++ unit1/memory.c
@@ -6,18 +6,16 @@ int main(void)
     int num = 42;                                   // 4 bytes
     char letter1 = 'A';                             // 1 byte
     char letter2 = 'B';                             // 1 byte
-    int nums[3] = { 1, 2, 3 };                      // 12 bytes
+    int num2 = 77;                                  // 4 bytes
     string word = "hello";                          // 6 bytes
     char school[] = "JHS";                          // 4 bytes
     char motto[4] = { 'A', 'M', 'D', 'G' };         // 4 bytes
-    bool answer = true;                             // 1 byte
 
     printf("%p\n", &num);
     printf("%p\n", &letter1);
     printf("%p\n", &letter2);
-    printf("%p\n", &nums);
+    printf("%p\n", &num2);
     printf("%p\n", &word);
     printf("%p\n", &school);
     printf("%p\n", &motto);
-    printf("%p\n", &answer);
 }
